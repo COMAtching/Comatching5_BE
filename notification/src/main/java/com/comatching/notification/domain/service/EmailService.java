c5876ab4bf6ada20529b5c85c3cf88294dd1b2ca
popeye <popeye0618@gmail.com>
2026-01-19 15:13:56 +0900

    refactor: Remove obsolete email service implementation
    - Deleted `EmailService` and `EmailServiceImpl` classes
    - Removed unused methods including `sendWelcomeEmail` and `sendWithdrawalEmail`
    - Simplified notification service by cleaning up legacy email logic

--- notification/src/main/java/com/comatching/notification/domain/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.comatching.notification.domain.service;
-
-public interface EmailService {
-
-	void sendWelcomeEmail(String toEmail, String nickname);
-	void sendWithdrawalEmail(String toEmail);
-}
